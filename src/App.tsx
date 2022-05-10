15d6e11d3084bd5280b2a427506c08ccbbea0ffa
ANGELUCCI Humberto <humberto.angelucci@soprasteria.com>
2022-05-10 15:38:43 +0200

    init repo

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import gif from '../src/assets/giphy-gif-2.webp'
+
+function App() {
+
+
+  return (
+    <div style={{display: 'flex', justifyContent: 'center', alignItems:'cenetr'}}>
+
+      <img width={1600} src={gif} alt="gif" />
+  
+    
+    </div>
+  )
+}
+
+export default App
